01f9a734de050ce8e86d2735846f0d14c3bca5f2
Fares Kato <fareskato@gmail.com>
2017-02-26 20:35:33 +0300

    variables ,strings interpolation and data types

---
+++ string_interpolation.ts
@@ -0,0 +1,5 @@
+let theName : string = "student";
+let theAge  : number= 38;
+console.log(`Hello ${theName} Your age is : ${theAge}`);
+/* And also U can use the traditional js interpolation */
+console.log("welcome " + theName + " the age is " + theAge);
